39a2787df23d46a90f5f99a38fc04395a48d4c9d
lispy2010 <serlysus@hotmail.com>
2022-07-02 12:00:52 +0300

    Convert small planks image

---
+++ build/planks_small_img.h
@@ -0,0 +1,24 @@
+
+//{{BLOCK(planks_small)
+
+//======================================================================
+//
+//	planks_small, 8x8@16, 
+//	+ bitmap not compressed
+//	Total size: 128 = 128
+//
+//	Time-stamp: 2022-07-02, 12:00:30
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.17
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_PLANKS_SMALL_H
+#define GRIT_PLANKS_SMALL_H
+
+#define planks_smallBitmapLen 128
+extern const unsigned int planks_smallBitmap[32];
+
+#endif // GRIT_PLANKS_SMALL_H
+
+//}}BLOCK(planks_small)
