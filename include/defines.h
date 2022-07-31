3b5324a14ba5590c2be90724706a80279cb723e2
lispy2010 <koronavirusnyj@gmail.com>
2022-07-31 10:24:51 +0300

    Update version

--- include/defines.h
+++ include/defines.h
@@ -30,8 +30,8 @@
     }
 
 #define VERSION_PREFIX "alpha"
-#define VERSION_MAJOR 1
-#define VERSION_MINOR 31
+#define VERSION_MAJOR 2
+#define VERSION_MINOR 0
 #define VERSION_PATCH 0
 #define SKIP_SPLASH_SCREEN 0
 
