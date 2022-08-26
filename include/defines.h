aa4659eb72d5b32926ab47e9c51077d60193b388
IAmMoltony <koronavirusnyj@gmail.com>
2022-08-26 13:55:26 +0600

    Update version

--- include/defines.h
+++ include/defines.h
@@ -34,8 +34,8 @@
 
 #define VERSION_PREFIX "alpha"
 #define VERSION_MAJOR 2
-#define VERSION_MINOR 1
-#define VERSION_PATCH 2
+#define VERSION_MINOR 2
+#define VERSION_PATCH 0
 #define SKIP_SPLASH_SCREEN 0
 #define FS_ERROR_MESSAGES 0
 
