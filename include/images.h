f641f643076f94d4f0a170be756f7723beaf5a72
IAmMoltony <koronavirusnyj@gmail.com>
2022-10-03 06:46:03 +0300

    Add chest textures and organize images.h

--- include/images.h
+++ include/images.h
@@ -1,6 +1,8 @@
 // leaves textures suffixed with _a are transparent
 
 #pragma once
+
+// blocks
 #include "cactus_side.h"
 #include "dandelion.h"
 #include "red_tulip.h"
@@ -9,8 +11,6 @@
 #include "door.h"
 #include "birchdoor.h"
 #include "grass.h"
-#include "inventory_slot.h"
-#include "inventory_slot_select.h"
 #include "oak_leaves.h"
 #include "oak_leaves_a.h"
 #include "birch_leaves.h"
@@ -30,16 +30,23 @@
 #include "bedrock.h"
 #include "cobblestone.h"
 #include "coal_ore.h"
-#include "coal.h"
 #include "coal_block.h"
 #include "glass.h"
 #include "oak_trapdoor.h"
 #include "birch_trapdoor.h"
 #include "ladder.h"
+#include "chest.h"
+
+// placeholder textures
 #include "placeholder_logo.h"
 #include "placeholder_guy.h"
+#include "dummy.h"
+
+// player textures
 #include "steve.h"
 #include "steve_head.h"
+
+// gui
 #include "logo.h"
 #include "abtn.h"
 #include "bbtn.h"
@@ -50,18 +57,28 @@
 #include "world_label_select.h"
 #include "gray_circle.h"
 #include "dirent_games.h"
+#include "english.h"
+#include "russian.h"
+
+// entities
 #include "pig.h"
 #include "pig_damage.h"
+
+// items
 #include "porkchop.h"
 #include "cooked_porkchop.h"
+#include "apple.h"
+#include "coal.h"
+
+// player gui
 #include "heart_outline.h"
 #include "half_heart.h"
 #include "half_heart2.h"
 #include "aim.h"
-#include "dummy.h"
-#include "apple.h"
-#include "english.h"
-#include "russian.h"
+#include "inventory_slot.h"
+#include "inventory_slot_select.h"
+
+// fonts
 #include "font_16x16.h"
 #include "font_small.h"
 #include "font_small_ru1.h"
