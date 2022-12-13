72e2f1e41a7184fe16fee29e963ed02db5749591
moltony <koronavirusnyj@gmail.com>
2022-12-13 19:57:07 +0600

    Add animated sprite

---
+++ include/animsprite.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <gl2d.h>
+#include <stdio.h>
+#include <limits.h>
+#include <vector>
+#include <initializer_list>
+
+enum class AnimatedSpriteMode
+{
+    Normal,
+    ReverseLoop,
+};
+
+class AnimatedSprite
+{
+private:
+    std::vector<glImage *> frames;
+    u8 img, frame, framesPerImage, reverseLoopDirection;
+    AnimatedSpriteMode mode;
+
+public:
+    AnimatedSprite(const AnimatedSprite &) = delete;
+
+    AnimatedSprite(u8 framesPerImage, AnimatedSpriteMode mode,
+                   std::initializer_list<glImage *> images);
+
+    void update(void);
+    void draw(int x, int y, GL_FLIP_MODE flipMode);
+    void draw(int x, int y);
+};
---
+++ source/animsprite.cpp
@@ -0,0 +1,52 @@
+#include "animsprite.hpp"
+
+AnimatedSprite::AnimatedSprite(u8 framesPerImage, AnimatedSpriteMode mode,
+                               std::initializer_list<glImage *> images)
+    : frames(std::vector<glImage *>(images)), img(0), frame(0),
+      framesPerImage(framesPerImage), reverseLoopDirection(1), mode(mode)
+{
+    if (frames.size() > SCHAR_MAX)
+    {
+        printf("Too many frames (expected <=%d, got %u)", SCHAR_MAX, frames.size());
+        while (true)
+            ;
+    }
+}
+
+void AnimatedSprite::update(void)
+{
+    ++frame;
+    if (frame == framesPerImage)
+    {
+        frame = 0;
+        switch (mode)
+        {
+        case AnimatedSpriteMode::Normal:
+            ++img;
+            if (img >= frames.size())
+                img = 0;
+            break;
+        case AnimatedSpriteMode::ReverseLoop:
+            if (reverseLoopDirection == 0)
+                --img;
+            else
+                ++img;
+
+            if (img == frames.size() - 1)
+                reverseLoopDirection = 0;
+            else if (img == 0)
+                reverseLoopDirection = 1;
+            break;
+        }
+    }
+}
+
+void AnimatedSprite::draw(int x, int y, GL_FLIP_MODE flipMode)
+{
+    glSprite(x, y, flipMode, frames[img]);
+}
+
+void AnimatedSprite::draw(int x, int y)
+{
+    draw(x, y, GL_FLIP_NONE);
+}
