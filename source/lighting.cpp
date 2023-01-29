9c47aa20c8d3c67a49a1a6e5e6986f3abd34278e
moltony <koronavirusnyj@gmail.com>
2023-01-29 15:13:13 +0600

    Rewrite lighting

--- source/lighting.cpp
+++ source/lighting.cpp
@@ -1,48 +1,36 @@
 #include "lighting.hpp"
 
-constexpr u16 WORLD_END = SCREEN_WIDTH * 2 / 16 + SCREEN_WIDTH * 2 / 16;
+constexpr u16 WORLD_END = (SCREEN_WIDTH * 2 / 16) * 2;
 
-u16 blockInstanceMeeting(BlockList *blocks, Rect updateArea, s16 x, s16 y,
-                         bool checkNonSolid = false)
+static u16 findBlock(BlockList *blocks, Rect findArea, s16 x, s16 y,
+                                        bool checkNonSolid = false)
 {
-    bool updateAll = updateArea.x == -1 && updateArea.y == -1 && updateArea.w == -1 &&
-                     updateArea.h == -1;
-
-    updateArea.x -= 24;
-    updateArea.y -= 16;
-    updateArea.w += 48;
-    updateArea.h += 32;
-
-    if (x < 0 || x > WORLD_END * 16)
-        return UINT16_MAX;
+    bool updateAll = findArea.x == -1 && findArea.y == -1 && findArea.w == -1 && findArea.h == -1;
 
     for (auto &block : *blocks)
     {
         if (!updateAll)
         {
-            if (block->getRect().x - updateArea.x < 0 ||
-                block->getRect().y - updateArea.y < 0)
+            // skip blocks out of camera
+            if (block->getRect().x - findArea.x < -16 ||
+                block->getRect().y - findArea.y < -16)
                 continue;
-            if (!block->getRect().intersects(updateArea))
+            if (block->getRect().x - findArea.x > SCREEN_WIDTH + 48)
                 break;
         }
 
         if (!checkNonSolid && !block->solid())
             continue;
 
-        if (block->getRect().isPointInside(x, y))
+        if (block->x == x && block->y == y)
             return block->id();
     }
 
     return 0;
 }
 
-// TODO fix this thing
-
 void updateLighting(BlockList *blocks, Rect updateArea)
 {
-    return;
-
     bool updateAll = updateArea.x == -1 && updateArea.y == -1 && updateArea.w == -1 &&
                      updateArea.h == -1;
 
@@ -50,22 +38,30 @@ void updateLighting(BlockList *blocks, Rect updateArea)
     {
         if (!updateAll)
         {
-            if (block->getRect().x - updateArea.x < 0 ||
-                block->getRect().y - updateArea.y < 0)
+            // skip blocks out of camera
+            if (block->getRect().x - updateArea.x < -16 ||
+                block->getRect().y - updateArea.y < -16)
                 continue;
-            if (!block->getRect().intersects(updateArea))
+            if (block->getRect().x - updateArea.x > SCREEN_WIDTH + 48)
                 break;
         }
 
-        if (blockInstanceMeeting(blocks, updateArea, block->x + 2, block->y - 15) &&
-            blockInstanceMeeting(blocks, updateArea, block->x + 2, block->y + 18) &&
-            blockInstanceMeeting(blocks, updateArea, block->x - 2, block->y + 2) &&
-            blockInstanceMeeting(blocks, updateArea, block->x + 18, block->y + 2))
+        Rect findArea = Rect(-1, -1, -1, -1);
+        if (!updateAll)
+            findArea = Rect(block->x - 16, block->y - 64, 64, 128);
+
+        if (findBlock(blocks, findArea, block->x, block->y - 16) &&
+            findBlock(blocks, findArea, block->x, block->y + 16) &&
+            findBlock(blocks, findArea, block->x - 16, block->y) &&
+            findBlock(blocks, findArea, block->x + 16, block->y))
         {
-            if (blockInstanceMeeting(blocks, updateArea, block->x + 2, block->y - 47))
-                block->lightLevel = 3;
-            else if (blockInstanceMeeting(blocks, updateArea, block->x + 2, block->y - 31))
-                block->lightLevel = 2;
+            if (findBlock(blocks, findArea, block->x, block->y - 32))
+            {
+                if (findBlock(blocks, findArea, block->x, block->y - 48))
+                    block->lightLevel = 3;
+                else
+                    block->lightLevel = 2;
+            }
             else
                 block->lightLevel = 1;
         }
