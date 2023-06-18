53b82d97abc3151088431cffbf3d761bc9ebb93b
moltony <koronavirusnyj@gmail.com>
2023-06-18 16:45:43 +0600

    Add stats files

---
+++ include/stats.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "save.hpp"
+
+void statsSetWorld(const std::string &worldName);
+float statsGetEntry(const std::string &entryKey);
+void statsSetEntry(const stdLLstring &entryKey, float value);
---
+++ source/stats.cpp
@@ -0,0 +1,13 @@
+#include "stats.hpp"
+
+void statsSetWorld(const std::string &worldName)
+{
+}
+
+float statsGetEntry(const std::string &entryKey)
+{
+}
+
+void statsSetEntry(const std::string &entryKey, float value)
+{
+}
