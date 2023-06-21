ebee45eb3a34befcb396441ff98b4e4c52530b27
moltony <koronavirusnyj@gmail.com>
2023-06-21 13:40:44 +0600

    Update defines.h to be better tbh

--- include/defines.h
+++ include/defines.h
@@ -1,17 +1,21 @@
 #pragma once
+
 #ifdef __cplusplus
+// image loading functions
 #define loadImage(spr, w, h, bitmap) glLoadTileSet(spr, w, h, w, h, GL_RGB, TEXTURE_SIZE_##w, TEXTURE_SIZE_##h, GL_TEXTURE_WRAP_S | GL_TEXTURE_WRAP_T | TEXGEN_OFF, 0, 0, const_cast<u8 *>(reinterpret_cast<const u8 *>(bitmap)))
 #define loadImageAlpha(spr, w, h, pal, bitmap) glLoadTileSet(spr, w, h, w, h, GL_RGB16, TEXTURE_SIZE_##w, TEXTURE_SIZE_##h, GL_TEXTURE_WRAP_S | GL_TEXTURE_WRAP_T | TEXGEN_OFF | GL_TEXTURE_COLOR0_TRANSPARENT, 16, pal, const_cast<u8 *>(reinterpret_cast<const u8 *>(bitmap)))
-#else
-#define loadImage(spr, w, h, bitmap) glLoadTileSet(spr, w, h, w, h, GL_RGB, TEXTURE_SIZE_##w, TEXTURE_SIZE_##h, GL_TEXTURE_WRAP_S | GL_TEXTURE_WRAP_T | TEXGEN_OFF, 0, 0, (u8 *)bitmap)
-#define loadImageAlpha(spr, w, h, pal, bitmap) glLoadTileSet(spr, w, h, w, h, GL_RGB16, TEXTURE_SIZE_##w, TEXTURE_SIZE_##h, GL_TEXTURE_WRAP_S | GL_TEXTURE_WRAP_T | TEXGEN_OFF | GL_TEXTURE_COLOR0_TRANSPARENT, 16, pal, (u8 *)bitmap)
-#endif
-#define unloadImage(spr) glDeleteTextures(1, &spr->textureID)
-#ifdef __cplusplus
+
 #define randomRange(a, b) ((a) + randomGenerate() % (((b) + 1) - (a)))
 #else
+// image loading functions
+#define loadImage(spr, w, h, bitmap) glLoadTileSet(spr, w, h, w, h, GL_RGB, TEXTURE_SIZE_##w, TEXTURE_SIZE_##h, GL_TEXTURE_WRAP_S | GL_TEXTURE_WRAP_T | TEXGEN_OFF, 0, 0, (u8 *)bitmap)
+#define loadImageAlpha(spr, w, h, pal, bitmap) glLoadTileSet(spr, w, h, w, h, GL_RGB16, TEXTURE_SIZE_##w, TEXTURE_SIZE_##h, GL_TEXTURE_WRAP_S | GL_TEXTURE_WRAP_T | TEXGEN_OFF | GL_TEXTURE_COLOR0_TRANSPARENT, 16, pal, (u8 *)bitmap)
+
 #define randomRange(a, b) ((a) + rand() % (((b) + 1) - (a)))
 #endif
+
+#define unloadImage(spr) glDeleteTextures(1, &spr->textureID)
+
 #define soundEffect(id)                            \
     {                                              \
         {id}, (int)(1.0f * (1 << 10)), 0, 255, 128 \
@@ -66,8 +70,7 @@
 #define SCALE(times) (SCALE_NORMAL * (times))
 #define HALFSIZE ((1 << 12) / 2)
 
-// game data dirs
-
+// game data directories
 #define DATA_DIR "fat:/dscraft_data"
 #define WORLDS_DIR DATA_DIR "/worlds"
 #define CONFIG_DIR DATA_DIR "/config"
