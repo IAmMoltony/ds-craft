44c856fdcd101234091dea95ae818299b16c9ad4
moltony <koronavirusnyj@gmail.com>
2023-07-23 17:33:48 +0600

    Undo create-release.py improvements

--- create-release.py
+++ create-release.py
@@ -4,30 +4,21 @@ import subprocess
 import shutil
 import pathlib
 
-
 def get_game_version():
+    version = ""
+    prefix = ""
     with open("nitrofs/game.ver", "r") as f:
-        data = f.read().splitlines()[:4]
-        version = ".".join(data[:2])
-        prefix = data[3]
+        data = f.read()
+        data_split = data.split('\n')
+        for i in range(4):
+            line = data_split[i]
+            if i == 0 or i == 1:
+                version += f"{line}."
+            elif i == 2:
+                version += line
+            elif i == 3:
+                prefix = line
     return f"{prefix}{version}"
-
-
-def build_project(clean=True, quiet=False):
-    if not clean:
-        subprocess.run(["make", "clean"], stdout=subprocess.DEVNULL)
-
-    make_command = ["make"]
-    if quiet:
-        make_command.append("-s")
-
-    subprocess.run(make_command, check=True)
-
-
-def save_release_file(version):
-    shutil.copyfile("bin/ds-craft.nds", f"releases/ds-craft-{version}.nds")
-    print(f"Saved to releases/ds-craft-{version}.nds")
-
 
 def main():
     pathlib.Path("releases").mkdir(parents=True, exist_ok=True)
@@ -37,13 +28,24 @@ def main():
     parser.add_argument("--quiet", help="don't display output (except for errors)", action="store_true")
     args = parser.parse_args()
 
-    no_clean = args.noclean
     be_quiet = args.quiet
+    no_clean = args.noclean
 
-    build_project(clean=not no_clean, quiet=be_quiet)
-    game_version = get_game_version()
-    save_release_file(game_version)
+    if not no_clean:
+        if be_quiet:
+            subprocess.run(["make", "clean"], stdout=subprocess.DEVNULL)
+        else:
+            print("Running `make clean'")
+            subprocess.run(["make", "clean"])
 
+    if be_quiet:
+        subprocess.run(["make"], check=True, stdout=subprocess.DEVNULL)
+    else:
+        print("Running `make'")
+        subprocess.run(["make"], check=True)
+
+    shutil.copyfile("bin/ds-craft.nds", f"releases/ds-craft-{get_game_version()}.nds")
+    print(f"Saved to releases/ds-craft-{get_game_version()}.nds")
 
 if __name__ == "__main__":
     main()
