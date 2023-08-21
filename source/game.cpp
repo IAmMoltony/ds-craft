514cbada023b81e561df53062909ba2c340cb955
IAmMoltony <koronavisurnyj@gmail.com>
2023-08-21 12:34:54 +0600

    Add timing crafting code

--- source/game.cpp
+++ source/game.cpp
@@ -324,9 +324,12 @@ void Game::init(void)
     fsCreateDir(CONFIG_DIR);
 
     printf("Initializing crafting\n");
+    cpuStartTiming(0);
 
     // init crafting
     Player::initCrafting();
+
+    printf("Took %f seconds\n", (float)cpuEndTiming() / BUS_CLOCK);
 
     printf("Loading assets\n");
 
