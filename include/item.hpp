098c5064264ef0ac660bf85f9044c6bc04de5d21
IAmMoltony <koronavisurnyj@gmail.com>
2023-08-27 22:42:48 +0600

    Add todo

--- include/item.hpp
+++ include/item.hpp
@@ -102,5 +102,5 @@ public:
 };
 
 std::string iidToString(InventoryItem::ID iid);
-const char *getItemStr(Language lang, InventoryItem::ID iid);
+const char *getItemStr(Language lang, InventoryItem::ID iid); // TODO rename this to getItemName or smth smh
 glImage *getItemImage(InventoryItem::ID item);
