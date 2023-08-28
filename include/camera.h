f8cfa1ca25133c9f17afeaad5e3292f1a5a31b9d
IAmMoltony <koronavisurnyj@gmail.com>
2023-08-28 18:31:22 +0600

    Add docs to camera

--- include/camera.h
+++ include/camera.h
@@ -1,6 +1,17 @@
 #pragma once
 
+/**
+ * @brief Structure for the game camera
+*/
 typedef struct camera
 {
-    int x, y;
+    /**
+     * @brief X position of the camera
+    */
+    int x;
+
+    /**
+     * @brief Y position of the camera
+    */
+    int y;
 } Camera;
