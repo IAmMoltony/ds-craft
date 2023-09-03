8b5db0d59204543ca592bc116f45cedba1e0b4d0
IAmMoltony <koronavirusnyj@gmail.com>
2023-09-03 11:58:37 +0600

    [TODO fix] eh

--- include/crafting.hpp
+++ include/crafting.hpp
@@ -1,7 +1,7 @@
 /**
  * @file crafting.hpp
  * @brief Crafting recipe class
-*/
+ */
 
 #pragma once
 
@@ -11,34 +11,34 @@
 
 /**
  * @brief Class representing a crafting recipe
-*/
+ */
 class CraftingRecipe
 {
 private:
     /**
      * @brief Output item count
-    */
+     */
     u8 count;
 
     /**
      * @brief Output item ID
-    */
-    InventoryItem::ID output; // TODO output can just be an InventoryItem and the count will be the output's amount
+     */
+    InventoryItem::ID output;
 
     /**
      * @brief The file name of the recipe
-    */
+     */
     std::string fileName;
 
     /**
      * @brief List of items needed to craft the item
-    */
+     */
     std::vector<InventoryItem> recipe;
 
     /**
      * @brief Construct a crafting recipe
      * @param recipeFile recipe file
-    */
+     */
     void construct(const char *recipeFile);
 
 public:
@@ -47,13 +47,13 @@ public:
     /**
      * @brief Crafting recipe constructor (using const char *)
      * @param recipeFile file to load the recipe from
-    */
+     */
     explicit CraftingRecipe(const char *recipeFile);
 
     /**
      * @brief Crafting recipe constructor (using const string reference)
      * @param recipeFile file to load the recipe from
-    */
+     */
     explicit CraftingRecipe(const std::string &recipeFile);
 
     /**
@@ -61,29 +61,29 @@ public:
      * @param player player (used for item count in the inventory)
      *
      * The full name is used in the crafting recipe screen
-    */
+     */
     std::string getFullName(Player *player);
 
     /**
      * @brief Get recipe file name
      * @see fileName
-    */
+     */
     std::string getFileName(void);
 
     /**
      * @brief Get output item count
      * @see count
-    */
+     */
     u8 getCount(void);
 
     /**
      * @brief Get output item ID
      * @see output
-    */
+     */
     InventoryItem::ID getOutput(void);
 
     /**
      * @brief Get list of items to craft the recipe
-    */
+     */
     std::vector<InventoryItem> *getRecipe(void);
 };
