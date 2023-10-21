cc6756ee28351888686f5f0f870dce6770bb9f52
IAmMoltony <koronavirusnyj@gmail.com>
2023-10-21 20:52:23 +0600

    Add PCX image docs

--- include/pcximage.h
+++ include/pcximage.h
@@ -8,9 +8,19 @@
 #include <nds.h>
 #include <gl2d.h>
 
+/**
+ * @brief Structure for storing images loaded from PCX files
+ */
 typedef struct
 {
+	/**
+	 * @brief Image returned by pcxLoad
+	 */
 	sImage simg;
+
+	/**
+	 * @brief Image in format that GL2D understands so we can draw it
+	 */
 	glImage spr[1];
 } PCXImage;
 
@@ -19,9 +29,44 @@ extern "C"
 {
 #endif
 
+	/**
+	 * @brief Load image from PCX file
+	 *
+	 * @param filePath Path to PCX file
+	 * @param image PCX image to load into
+	 */
 	void pcxImageLoad(const char *filePath, PCXImage *image);
+
+	/**
+	 * @brief Unload a PCX image
+	 *
+	 * @param image Image to unload
+	 */
 	void pcxImageUnload(PCXImage *image);
+
+	/**
+	 * @brief Draw a PCX image
+	 *
+	 * @param image PCX image to draw
+	 * @param x X position of the image
+	 * @param y Y position of the image
+	 * @param flip How the image should be flipped (use `GL_FLIP_NONE` for no flip)
+	 */
 	void pcxImageDraw(PCXImage *image, int x, int y, GL_FLIP_MODE flip);
+
+	/**
+	 * @brief Draw a PCX image (advanced edition)
+	 *
+	 * @param image PCX image to draw
+	 * @param x X position of the image
+	 * @param y Y position of the image
+	 * @param xOff image X offset
+	 * @param yOff image Y offset
+	 * @param w image width
+	 * @param h image height
+	 * @param scale image scaling
+	 * @param flip how the image should be flipped
+	 */
 	void pcxImageDrawEx(PCXImage *image, int x, int y, int xOff, int yOff, int w, int h, s32 scale, GL_FLIP_MODE flip);
 
 #ifdef __cplusplus
