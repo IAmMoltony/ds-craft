d446cb724f4a844423f80a53a2ed40427202146b
Moltony <koronavirusnyj@gmail.com>
2023-12-02 11:59:23 +0600

    Add PowerShell version of lstodos

---
+++ scripts/lstodos.ps1
@@ -0,0 +1,6 @@
+$todoPattern = "@todo|TODO"
+
+Get-Content .\source\* | Select-String -Pattern $todoPattern 
+Get-Content .\include\* | Select-String -Pattern $todoPattern
+Get-Content .\scripts\*.py | Select-String -Pattern $todoPattern
+Get-Content .\README.md | Select-String -Pattern $todoPattern
