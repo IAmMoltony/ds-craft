313a90bde811506923fb4ddb17d7d144babc3418
IAmMoltony <koronavirusnyj@gmail.com>
2024-01-09 15:11:40 +0600

    Update docs script

--- docs.sh
+++ docs.sh
@@ -1,2 +1,14 @@
-#!/bin/sh
-doxygen && cat doxygen-warn.log
+#!/usr/bin//bash
+
+echo "Running Doxygen"
+doxygen
+
+if [ "$?" != "0" ]; then
+    echo "Doxygen error, please see log above"
+    exit 1
+fi
+
+clear
+echo "Doxygen run successfully, warning log:"
+cat doxygen-warn.log
+
