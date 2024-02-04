70ebf924a057098422bab516b7eda226a3fb9142
Moltony <105192532+IAmMoltony@users.noreply.github.com>
2024-02-04 14:31:37 +0600

    genworldmeta: Readability

--- scripts/genworldmeta.py
+++ scripts/genworldmeta.py
@@ -10,23 +10,23 @@ from create_release import get_game_version
 def main():
     """Main function"""
     print("World metadata generator")
-    wn = input("Enter world name >")
-    wv = input("Enter world version (leave blank to use current) >")
-    ws = input("Enter world seed (leave blank for random) >")
-    sd = 0
+    world_name = input("Enter world name >")
+    world_version = input("Enter world version (leave blank to use current) >")
+    world_seed = input("Enter world seed (leave blank for random) >")
+    seed_num = 0
 
-    if not wv:
-        wv = get_game_version()
-    if not ws:
-        sd = random.randint(0, 2**32 - 2)
+    if not world_version:
+        world_version = get_game_version()
+    if not world_seed:
+        seed_num = random.randint(0, 2**32 - 2)
     else:
-        sd = hash(ws)
+        world_seed = hash(world_seed)
 
     print("Here's your world.meta file:")
     print()
-    print(f"worldname {wn}")
-    print(f"gameversion {wv}")
-    print(f"seed {sd}")
+    print(f"worldname {world_name}")
+    print(f"gameversion {world_version}")
+    print(f"seed {seed_num}")
     print("location 0")
     print()
     print("Please paste the above text into a `world.meta` file in the world folder.")
